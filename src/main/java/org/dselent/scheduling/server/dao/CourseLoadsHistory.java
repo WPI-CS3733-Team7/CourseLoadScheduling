23b09c7a524cfa9f8d681d857328b39e9bbdfed6
jlanson <jlanson@130.215.223.115>
2018-01-30 20:32:50 -0500

    Add courseloaddao, courseloadshistory, isntructorhistorydao interfaces

---
+++ src/main/java/org/dselent/scheduling/server/dao/CourseLoadsHistory.java
@@ -0,0 +1,8 @@
+package org.dselent.scheduling.server.dao;
+
+import org.dselent.scheduling.server.model.CourseLoadHistory;
+
+public interface CourseLoadsHistory extends Dao<CourseLoadHistory>
+{
+	// add functions as needed
+}
---
+++ src/main/java/org/dselent/scheduling/server/dao/InstructorsHistoryDao.java
@@ -0,0 +1,8 @@
+package org.dselent.scheduling.server.dao;
+
+import org.dselent.scheduling.server.model.InstructorHistory;
+
+public interface InstructorsHistoryDao extends Dao<InstructorHistory>
+{
+	// add functions here as needed
+}
