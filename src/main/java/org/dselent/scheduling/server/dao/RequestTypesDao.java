9737e2d9d6dea69c532ff1575d80ea91830a101d
jlharris <justinlharris15@gmail.com>
2018-01-31 14:46:10 -0500

    Adding daos

---
+++ src/main/java/org/dselent/scheduling/server/dao/RequestTypesDao.java
@@ -0,0 +1,8 @@
+package org.dselent.scheduling.server.dao;
+
+import org.dselent.scheduling.server.model.RequestType;
+
+public interface RequestTypesDao extends Dao<RequestType> 
+{
+	//add functions here as needed
+}
---
+++ src/main/java/org/dselent/scheduling/server/dao/RequestsDao.java
@@ -0,0 +1,8 @@
+package org.dselent.scheduling.server.dao;
+
+import org.dselent.scheduling.server.model.Request;
+
+public interface RequestsDao extends Dao<Request>
+{
+	//add functions here as needed
+}
