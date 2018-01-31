8c8efa1ecce5b29c8e8f8e507bfb5bda6973372b
John Amaral <john3r8amaral@yahoo.com>
2018-01-31 16:39:02 -0500

    Added InstructorsDaoImpl.java, minor changes to Instructors.java

--- src/main/java/org/dselent/scheduling/server/model/Instructor.java
+++ src/main/java/org/dselent/scheduling/server/model/Instructor.java
@@ -171,127 +171,73 @@ public class Instructor extends Model {
     }
 
     @Override
-    public int hashCode()
-    {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((createdAt == null) ? 0 : createdAt.hashCode());
-        result = prime * result + ((email == null) ? 0 : email.hashCode());
-        result = prime * result + ((rank == null) ? 0 : rank.hashCode());
-        result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
-        result = prime * result + ((updatedAt == null) ? 0 : updatedAt.hashCode());
-        result = prime * result + ((deleted == null) ? 0 : deleted.hashCode());
-        return result;
-    }
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((createdAt == null) ? 0 : createdAt.hashCode());
+		result = prime * result + ((deleted == null) ? 0 : deleted.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + ((rank == null) ? 0 : rank.hashCode());
+		result = prime * result + ((updatedAt == null) ? 0 : updatedAt.hashCode());
+		return result;
+	}
 
-    @Override
-    public boolean equals(Object obj)
-    {
-        if (this == obj)
-        {
-            return true;
-        }
-        if (obj == null)
-        {
-            return false;
-        }
-        if (!(obj instanceof Instructor))
-        {
-            return false;
-        }
-        Instructor other = (Instructor) obj;
-        if (createdAt == null)
-        {
-            if (other.createdAt != null)
-            {
-                return false;
-            }
-        }
-        else if (!createdAt.equals(other.createdAt))
-        {
-            return false;
-        }
-        if (email == null)
-        {
-            if (other.email != null)
-            {
-                return false;
-            }
-        }
-        else if (!email.equals(other.email))
-        {
-            return false;
-        }
-        if (firstName == null)
-        {
-            if (other.firstName != null)
-            {
-                return false;
-            }
-        }
-        else if (!firstName.equals(other.firstName))
-        {
-            return false;
-        }
-        if (id == null)
-        {
-            if (other.id != null)
-            {
-                return false;
-            }
-        }
-        else if (!id.equals(other.id))
-        {
-            return false;
-        }
-        if (lastName == null)
-        {
-            if (other.lastName != null)
-            {
-                return false;
-            }
-        }
-        else if (!lastName.equals(other.lastName))
-        {
-            return false;
-        }
-        if (rank == null)
-        {
-            if (other.rank != null)
-            {
-                return false;
-            }
-        }
-        else if (!rank.equals(other.rank))
-        {
-            return false;
-        }
-        if (updatedAt == null)
-        {
-            if (other.updatedAt != null)
-            {
-                return false;
-            }
-        }
-        else if (!updatedAt.equals(other.updatedAt))
-        {
-            return false;
-        }
-        if (deleted == null) {
-            if (other.deleted != null)
-                return false;
-        }
-        else if (!deleted.equals(other.deleted))
-        {
-            return false;
-        }
-        return true;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Instructor other = (Instructor) obj;
+		if (createdAt == null) {
+			if (other.createdAt != null)
+				return false;
+		} else if (!createdAt.equals(other.createdAt))
+			return false;
+		if (deleted == null) {
+			if (other.deleted != null)
+				return false;
+		} else if (!deleted.equals(other.deleted))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (rank == null) {
+			if (other.rank != null)
+				return false;
+		} else if (!rank.equals(other.rank))
+			return false;
+		if (updatedAt == null) {
+			if (other.updatedAt != null)
+				return false;
+		} else if (!updatedAt.equals(other.updatedAt))
+			return false;
+		return true;
+	}
 
-
-    @Override
+	@Override
     public String toString()
     {
         StringBuilder builder = new StringBuilder();
