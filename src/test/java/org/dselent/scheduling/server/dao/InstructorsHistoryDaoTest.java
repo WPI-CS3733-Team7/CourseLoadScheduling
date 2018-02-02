f9bd8713714d8546206b525c1000fc915467eb84
leifsahyun <lgsahyun@wpi.edu>
2018-02-01 21:05:37 -0500

    Instructor and InstructorHistory Tests

--- src/test/java/org/dselent/scheduling/server/dao/InstructorsHistoryDaoTest.java
+++ src/test/java/org/dselent/scheduling/server/dao/InstructorsHistoryDaoTest.java
@@ -6,9 +6,9 @@ import java.util.List;
 import java.util.Scanner;
 
 import org.dselent.scheduling.server.config.AppConfig;
-import org.dselent.scheduling.server.dao.InstructorsDao;
+import org.dselent.scheduling.server.dao.InstructorsHistoryDao;
 import org.dselent.scheduling.server.miscellaneous.Pair;
-import org.dselent.scheduling.server.model.Instructor;
+import org.dselent.scheduling.server.model.InstructorHistory;
 import org.dselent.scheduling.server.sqlutils.ColumnOrder;
 import org.dselent.scheduling.server.sqlutils.ComparisonOperator;
 import org.dselent.scheduling.server.sqlutils.QueryTerm;
@@ -22,40 +22,41 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {AppConfig.class})
 @WebAppConfiguration
-public class InstructorsDaoTest
+public class InstructorsHistoryDaoTest
 {
 	@Autowired
-	private InstructorsDao instructorsDao;
+	private InstructorsHistoryDao instructorsDao;
 	
 	/*
 	 * Not really an using this as a JUnit test
 	 * More of an example on how to use the classes
 	 */
     @Test
-    public void testInstructorsDao() throws SQLException
+    public void testInstructorHistorysDao() throws SQLException
     {
     	
     	Scanner scan = new Scanner(System.in);
     	
     	// INSERT
     	
-    	Instructor instructor1 = new Instructor();
+    	InstructorHistory instructor1 = new InstructorHistory();
     	instructor1.setFirstName("instructor");
     	instructor1.setLastName("one");
     	instructor1.setEmail("instructorone@wpi.edu");
     	instructor1.setRank("test proctor");; // simplified for now
+    	instructor1.setFormerId(1);
     	
     	List<String> insertColumnNameList = new ArrayList<>();
     	List<String> keyHolderColumnNameList = new ArrayList<>();
     	
-    	insertColumnNameList.add(Instructor.getColumnName(Instructor.Columns.FIRST_NAME));
-    	insertColumnNameList.add(Instructor.getColumnName(Instructor.Columns.LAST_NAME));
-    	insertColumnNameList.add(Instructor.getColumnName(Instructor.Columns.EMAIL));
-    	insertColumnNameList.add(Instructor.getColumnName(Instructor.Columns.RANK));
+    	insertColumnNameList.add(InstructorHistory.getColumnName(InstructorHistory.Columns.FIRST_NAME));
+    	insertColumnNameList.add(InstructorHistory.getColumnName(InstructorHistory.Columns.LAST_NAME));
+    	insertColumnNameList.add(InstructorHistory.getColumnName(InstructorHistory.Columns.EMAIL));
+    	insertColumnNameList.add(InstructorHistory.getColumnName(InstructorHistory.Columns.RANK));
+    	insertColumnNameList.add(InstructorHistory.getColumnName(InstructorHistory.Columns.FORMER_ID));
     	
-    	keyHolderColumnNameList.add(Instructor.getColumnName(Instructor.Columns.ID));
-    	keyHolderColumnNameList.add(Instructor.getColumnName(Instructor.Columns.CREATED_AT));
-    	keyHolderColumnNameList.add(Instructor.getColumnName(Instructor.Columns.UPDATED_AT));
+    	keyHolderColumnNameList.add(InstructorHistory.getColumnName(InstructorHistory.Columns.ID));
+    	keyHolderColumnNameList.add(InstructorHistory.getColumnName(InstructorHistory.Columns.CREATED_AT));
    	
     	instructorsDao.insert(instructor1, insertColumnNameList, keyHolderColumnNameList);
     	
@@ -63,51 +64,51 @@ public class InstructorsDaoTest
     	
     	// UPDATE
     	
-    	String updateColumnName = Instructor.getColumnName(Instructor.Columns.FIRST_NAME);
-    	String oldInstructorName = "instructor";
-    	String newInstructorName = "newInstructorName";
+    	String updateColumnName = InstructorHistory.getColumnName(InstructorHistory.Columns.FIRST_NAME);
+    	String oldInstructorHistoryName = "instructor";
+    	String newInstructorHistoryName = "newInstructorHistoryName";
     	List<QueryTerm> updateQueryTermList = new ArrayList<>();
     	
     	QueryTerm updateInstNameTerm = new QueryTerm();
     	updateInstNameTerm.setColumnName(updateColumnName);
     	updateInstNameTerm.setComparisonOperator(ComparisonOperator.EQUAL);
-    	updateInstNameTerm.setValue(oldInstructorName);
+    	updateInstNameTerm.setValue(oldInstructorHistoryName);
     	updateQueryTermList.add(updateInstNameTerm);
     	
-    	instructorsDao.update(updateColumnName, newInstructorName, updateQueryTermList);
+    	instructorsDao.update(updateColumnName, newInstructorHistoryName, updateQueryTermList);
     	
     	scan.next();
     	
     	// SELECT
     	// by instructor name
     	
-    	String selectColumnName = Instructor.getColumnName(Instructor.Columns.FIRST_NAME);
-    	String selectInstructorName = newInstructorName;
+    	String selectColumnName = InstructorHistory.getColumnName(InstructorHistory.Columns.FIRST_NAME);
+    	String selectInstructorHistoryHistoryName = newInstructorHistoryName;
     	
     	List<QueryTerm> selectQueryTermList = new ArrayList<>();
     	
     	QueryTerm selectInstNameTerm = new QueryTerm();
     	selectInstNameTerm.setColumnName(selectColumnName);
     	selectInstNameTerm.setComparisonOperator(ComparisonOperator.EQUAL);
-    	selectInstNameTerm.setValue(selectInstructorName);
+    	selectInstNameTerm.setValue(selectInstructorHistoryHistoryName);
     	selectQueryTermList.add(selectInstNameTerm);
     	
-    	List<String> selectColumnNameList = Instructor.getColumnNameList();
+    	List<String> selectColumnNameList = InstructorHistory.getColumnNameList();
     	
     	List<Pair<String, ColumnOrder>> orderByList = new ArrayList<>();
     	Pair<String, ColumnOrder> orderPair1 = new Pair<String, ColumnOrder>(selectColumnName, ColumnOrder.ASC);
     	orderByList.add(orderPair1);
     	
 		@SuppressWarnings("unused")
-		List<Instructor> selectedInstructorList = instructorsDao.select(selectColumnNameList, selectQueryTermList, orderByList);
+		List<InstructorHistory> selectedInstructorHistoryList = instructorsDao.select(selectColumnNameList, selectQueryTermList, orderByList);
 	
-		System.out.println(selectedInstructorList);
+		System.out.println(selectedInstructorHistoryList);
 		scan.next();
 		
 		// DELETE
 		
-		String deleteColumnName = Instructor.getColumnName(Instructor.Columns.FIRST_NAME);
-		String deleteInstName = newInstructorName;
+		String deleteColumnName = InstructorHistory.getColumnName(InstructorHistory.Columns.FIRST_NAME);
+		String deleteInstName = newInstructorHistoryName;
 		
 		List<QueryTerm> deleteQueryTermList = new ArrayList<>();
 		QueryTerm deleteInstNameTerm = new QueryTerm();
