842189067c76c3f7d3d291c67d8a2808a683b500
jlanson <jlanson@130.215.174.109>
2018-02-07 21:32:14 -0500

    wrote everything for register and login requests

--- src/main/java/org/dselent/scheduling/server/service/UserService.java
+++ src/main/java/org/dselent/scheduling/server/service/UserService.java
@@ -1,10 +1,8 @@
 package org.dselent.scheduling.server.service;
 
 import java.sql.SQLException;
-import java.util.List;
 
 import org.dselent.scheduling.server.dto.RegisterUserDto;
-import org.dselent.scheduling.server.model.User;
 import org.dselent.scheduling.server.returnobject.LoginUserReturnObject;
 import org.springframework.stereotype.Service;
 
@@ -26,6 +24,6 @@ public interface UserService
 	 * @return A list of rows affected for each insert operation
 	 * @throws SQLException
 	 */
-	public List<Integer> registerUser(RegisterUserDto registerUserDto) throws SQLException;
-    public LoginUserReturnObject loginUser(String userName, String password);
+	public String registerUser(RegisterUserDto registerUserDto) throws SQLException;
+    public LoginUserReturnObject loginUser(String userName, String password) throws SQLException;
 }
