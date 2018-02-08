7a32f68bb71f34758a81ab4d268273c637c4ee55
John Amaral <john3r8amaral@yahoo.com>
2018-02-08 16:42:48 -0500

    Fixed request mappings on InstructorsController

--- src/main/java/org/dselent/scheduling/server/controller/InstructorsController.java
+++ src/main/java/org/dselent/scheduling/server/controller/InstructorsController.java
@@ -2,7 +2,8 @@ package org.dselent.scheduling.server.controller;
 
 import java.util.Map;
 
-import org.dselent.scheduling.server.requests.Register;
+import org.dselent.scheduling.server.requests.InstructorEdit;
+import org.dselent.scheduling.server.requests.SelectInstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +12,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/scheduler/instructors")
 public interface InstructorsController
 {
-    @RequestMapping(method=RequestMethod.POST, value=Register.REQUEST_NAME)
+    @RequestMapping(method=RequestMethod.POST, value=SelectInstructor.REQUEST_NAME)
 	public ResponseEntity<String> select(@RequestBody Map<String, String> request) throws Exception;
 
-    @RequestMapping(method=RequestMethod.POST, value=Register.REQUEST_NAME)
+    @RequestMapping(method=RequestMethod.POST, value=InstructorEdit.REQUEST_NAME)
 	public ResponseEntity<String> edit(@RequestBody Map<String, String> request) throws Exception;
 }
 
