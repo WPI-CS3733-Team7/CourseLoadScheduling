f3ef8e4797ffb2213d0cb6e527d30a40e746fdbc
Marcus <marcuschan0311@gmail.com>
2018-02-09 18:34:43 -0500

    added toString and other methods to SelectCourseReturnObject

--- src/main/java/org/dselent/scheduling/server/returnobject/SelectCourseReturnObject.java
+++ src/main/java/org/dselent/scheduling/server/returnobject/SelectCourseReturnObject.java
@@ -8,16 +8,63 @@ import org.dselent.scheduling.server.model.CalendarInfo;
 public class SelectCourseReturnObject {
 	List<Instructor> instructorList;
 	List<CourseSection> courseSectionList;
-	List<CalendarInfo> calendarInfo;
+	List<CalendarInfo> calendarInfoList;
 	
 	public SelectCourseReturnObject(List<Instructor> instructorList, List<CourseSection> courseSectionList,
-			List<CalendarInfo> calendarInfo) {
+			List<CalendarInfo> calendarInfoList) {
 		
 		this.instructorList = instructorList;
 		this.courseSectionList = courseSectionList;
-		this.calendarInfo = calendarInfo;
+		this.calendarInfoList = calendarInfoList;
+	}
+	
+	@Override
+	public String toString() {
+		return "SelectInstructorReturnObject [instructorList=" + instructorList + ",\n courseSectionList=" + courseSectionList + ",\n calendarInfoList=" + calendarInfoList
+				+ "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((instructorList == null) ? 0 : instructorList.hashCode());
+		result = prime * result + ((courseSectionList == null) ? 0 : courseSectionList.hashCode());
+		result = prime * result + ((calendarInfoList == null) ? 0 : calendarInfoList.hashCode());
+		
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SelectCourseReturnObject other = (SelectCourseReturnObject) obj;
+		if(instructorList == null) {
+			if (other.instructorList != null)
+				return false;
+		}else if (calendarInfoList == null) {
+			if (other.calendarInfoList != null)
+				return false;
+		} else if (!calendarInfoList.equals(other.calendarInfoList))
+			return false;
+		if (courseSectionList == null) {
+			if (other.courseSectionList != null)
+				return false;
+		} else if (!courseSectionList.equals(other.courseSectionList))
+			return false;
+		return true;
 	}
 	
 	
-
+	
 }
+
+	
+	
+
+
