3d6ff2b00084e03d8d0664c34d080208705737d9
leifsahyun <lgsahyun@wpi.edu>
2018-02-08 19:41:56 -0500

    Fixed CourseController Error

--- src/main/java/org/dselent/scheduling/server/controller/CoursesController.java
+++ src/main/java/org/dselent/scheduling/server/controller/CoursesController.java
@@ -3,7 +3,8 @@ package org.dselent.scheduling.server.controller;
 import java.sql.SQLException;
 import java.util.Map;
 
-import org.dselent.scheduling.server.requests.Register;
+import org.dselent.scheduling.server.requests.CourseEdit;
+import org.dselent.scheduling.server.requests.SelectCourse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,10 +15,10 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 @RequestMapping("/scheduler/courses")
 public interface CoursesController
 {
-    @RequestMapping(method=RequestMethod.POST, value=Register.REQUEST_NAME)
+    @RequestMapping(method=RequestMethod.POST, value=SelectCourse.REQUEST_NAME)
 	public ResponseEntity<String> select(@RequestBody Map<String, String> request) throws Exception;
 
-    @RequestMapping(method=RequestMethod.POST, value=Register.REQUEST_NAME)
+    @RequestMapping(method=RequestMethod.POST, value=CourseEdit.REQUEST_NAME)
 	public ResponseEntity<String> edit(@RequestBody Map<String, String> request) throws SQLException, JsonProcessingException;
 }
 
