e3bfba860d04aa35e163cbcebe10806f1b784f7d
leifsahyun <lgsahyun@wpi.edu>
2018-02-09 17:53:48 -0500

    Added toString and other methods to SelectInstructorReturnObject

--- src/main/java/org/dselent/scheduling/server/returnobject/SelectInstructorReturnObject.java
+++ src/main/java/org/dselent/scheduling/server/returnobject/SelectInstructorReturnObject.java
@@ -12,6 +12,44 @@ public class SelectInstructorReturnObject {
 		this.sectionList = sectionList;
 		this.calendarInfoList = calendarInfoList;
 	}
+
+	@Override
+	public String toString() {
+		return "SelectInstructorReturnObject [sectionList=" + sectionList + ",\n calendarInfoList=" + calendarInfoList
+				+ "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((calendarInfoList == null) ? 0 : calendarInfoList.hashCode());
+		result = prime * result + ((sectionList == null) ? 0 : sectionList.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SelectInstructorReturnObject other = (SelectInstructorReturnObject) obj;
+		if (calendarInfoList == null) {
+			if (other.calendarInfoList != null)
+				return false;
+		} else if (!calendarInfoList.equals(other.calendarInfoList))
+			return false;
+		if (sectionList == null) {
+			if (other.sectionList != null)
+				return false;
+		} else if (!sectionList.equals(other.sectionList))
+			return false;
+		return true;
+	}
+	
 	
 	
 }
